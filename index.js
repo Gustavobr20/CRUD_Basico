ad9726d7b51083e5125b7ddfb21676488ab2eb83
Gu.henrique <gustavo.henrique.oliveira50@gmail.com>
2021-02-03 09:03:14 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require ('express')
+
+const server = express(); 
+
+server.use(express.json());
+
+// Query params = ?nome=NodeJS
+// Route params = /curso/2
+// Request Body = { nome: 'Node JS', tipo: 'Back-End'}
+
+const cursos = ['Node JS', 'javaScript', 'React Native'];
+
+server.get('/cursos', (req, res) => {
+    return res.json(cursos);
+});
+
+// com os comandos abaixo eu espero que ele acesse localhost:3000/cursos
+server.get('/cursos/:index', (req, res) =>{
+    const { index } = req.params;
+
+    return res.json(cursos[index]) // rota que tras informações, o segundo parametro é uma função que devolve alguma coisa e dentro do parenteses utilizado o req e o res
+}) 
+
+// criando um novo curso
+server.post('/cursos', (req, res) => {
+    const {name} = req.body;
+    cursos.push(name);
+
+    return res.json(cursos);
+});
+
+// Atualizando um curso
+server.put('/cursos/:index', (req, res) => {
+    const {index} = req.params;
+    const {name} = req.body;
+
+    cursos[index] = name;
+
+    return res.json(cursos);
+})
+
+// Excluindo algum curso
+
+server.delete('/cursos/:index', (req, res) => {
+    const {index} = req.params;
+
+    cursos.splice(index, 1);
+
+    return res.json(cursos);
+})
+
+server.listen(3000); // diz para o servidor ler a porta 3000
